fb2a6fb4887e3b69c1ab1b98e79ed73547339783
jorge <georgeramirez01@gmail.com>
2020-04-07 20:32:51 -0600

    FaceBookPush

--- NaNoRenO/Assets/Scripts/ChatManager.cs
+++ NaNoRenO/Assets/Scripts/ChatManager.cs
@@ -15,6 +15,9 @@ public class ChatManager : MonoBehaviour
         Text = gameObject.transform.GetChild(0).gameObject;
         boxBackground = GetComponent<RectTransform>();
         textBackground = Text.GetComponent<RectTransform>();
+        Debug.Log(Screen.width);
+        Text.GetComponent<TextMeshProUGUI>().fontSize = Screen.width * 0.009f;
+
     }
 
     // Update is called once per frame
---
+++ NaNoRenO/Assets/Scripts/TextAutoSize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextAutoSize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameObject.GetComponent<TextMeshProUGUI>().fontSize = Screen.width * 0.009f;
+
+    }
+}
