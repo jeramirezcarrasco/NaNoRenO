a0d8827763710039c3b22ad3222f88c670699f34
jorge <georgeramirez01@gmail.com>
2020-04-08 20:07:15 -0600

    Dekstop restore

---
+++ NaNoRenO/Assets/Scripts/NextScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public Animator transition;
+    public float TransitionTIme;
+    public bool Debugger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("m"))
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(TransitionTIme);
+        SceneManager.LoadScene(index);
+    }
+}
